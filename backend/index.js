d47680ffc7b187dd7d7fa659091984645dd3cda4
Rachit Agnihotri <agnihotrir@gmail.com>
2024-08-12 03:14:51 +0530

    Database and Routes Setup

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {dbConnection} = require('./Db/dbConn.js');
+app.use(cors);
+require('dotenv').config();
+app.use(express.json());//parse json
+
+//Db connection
+dbConnection();
+
+//routes
+app.use('/admin', require('./routes/Admin'));
+
+app.use('/user', require('./routes/User'));
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log('listening on port : ' + process.env.PORT);
+});
+
