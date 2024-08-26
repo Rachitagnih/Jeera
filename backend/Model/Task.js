da0e170ffc1ecaf95a48e941906d4c718e905c26
Rachit Agnihotri <agnihotrir@gmail.com>
2024-08-27 02:19:58 +0530

    Models and Relationship Setup

---
+++ backend/Model/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const taskSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project'
+    }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = {Task};
